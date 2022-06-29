247a2bd816cf8d872920a20c34cb621342cb7593
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2022-06-29 02:47:47 -0500

    App now loads pokemon picture and displays height

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -22,16 +22,25 @@ class HomeScreen extends StatelessWidget {
           var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$randNum/');
           var response = await http.get(url);
           //TODO: Do some error handling with the statusCode
-          //print('Response status: ${response.statusCode}');
-          
+          print('Response status: ${response.statusCode}');
+
           var data = jsonDecode(response.body);
           String pokemonName = data["forms"][0]["name"];
-          
+          int pokeHeight = data["height"];
+          String pokeImageUrl =
+              data["sprites"]["other"]["official-artwork"]["front_default"];
 
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) =>  SecondScreen(pokemonName: pokemonName,)));
+          print(pokeImageUrl);
+          print(pokeHeight);
+
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => SecondScreen(
+                    pokemonName: pokemonName,
+                    pokeHeight: pokeHeight,
+                    pokeImageUrl: pokeImageUrl,
+                  )));
         },
-        child: const Text("Go to next Screen"),
+        child: const Text("Get that Pokemon!"),
       ),
     )));
   }
--- lib/Screens/second_screen.dart
+++ lib/Screens/second_screen.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
- final String pokemonName;
-  const SecondScreen({Key? key, required this.pokemonName }) : super(key: key);
+  final String pokemonName;
+  final int pokeHeight;
+  final String pokeImageUrl;
+  const SecondScreen(
+      {Key? key,
+      required this.pokemonName,
+      required this.pokeHeight,
+      required this.pokeImageUrl})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-          body: Text(pokemonName),
-    ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(image: NetworkImage(pokeImageUrl)),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(pokemonName,
+                    style: const TextStyle(
+                      fontSize: 40,
+                      fontFamily: "Serif",
+                      fontWeight: FontWeight.w800,
+                    )),
+              ),
+              Text("Height: $pokeHeight feet",style:const TextStyle(fontSize: 25))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
