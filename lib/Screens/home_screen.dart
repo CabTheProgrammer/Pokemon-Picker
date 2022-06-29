5c45071fc563ad66730d3a4c32fec1d583348d42
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2022-06-29 03:36:15 -0500

    Fixed something ig

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -13,35 +13,34 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
             body: Center(
-      child: ElevatedButton(
-        onPressed: () async {
-          //Generate Random Number
-          var randNum = Random().nextInt(150);
+                child: ElevatedButton(
+      onPressed: () async {
+        //Generate Random Number
+        var randNum = Random().nextInt(150);
 
-          //Making Request to PokeApi
-          var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$randNum/');
-          var response = await http.get(url);
-          //TODO: Do some error handling with the statusCode
-          print('Response status: ${response.statusCode}');
+        //Making Request to PokeApi
+        var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$randNum/');
+        var response = await http.get(url);
+        //TODO: Do some error handling with the statusCode
+        // print('Response status: ${response.statusCode}');
 
-          var data = jsonDecode(response.body);
-          String pokemonName = data["forms"][0]["name"];
-          int pokeHeight = data["height"];
-          String pokeImageUrl =
-              data["sprites"]["other"]["official-artwork"]["front_default"];
+        var data = jsonDecode(response.body);
+        String pokemonName = data["forms"][0]["name"];
+        int pokeHeight = data["height"];
+        String pokeImageUrl =
+            data["sprites"]["other"]["official-artwork"]["front_default"];
 
-          print(pokeImageUrl);
-          print(pokeHeight);
-
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => SecondScreen(
-                    pokemonName: pokemonName,
-                    pokeHeight: pokeHeight,
-                    pokeImageUrl: pokeImageUrl,
-                  )));
-        },
-        child: const Text("Get that Pokemon!",style: TextStyle(fontSize: 25),
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => SecondScreen(
+                  pokemonName: pokemonName,
+                  pokeHeight: pokeHeight,
+                  pokeImageUrl: pokeImageUrl,
+                )));
+      },
+      child: const Text(
+        "Get that Pokemon!",
+        style: TextStyle(fontSize: 25),
       ),
-    )));
+    ))));
   }
 }
